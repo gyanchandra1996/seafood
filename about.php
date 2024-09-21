994369e616179bc0f7ea56eca9bf114e83e0a307
Gyan <gyanchanndranist@gmail.com>
2024-09-21 02:08:28 -0700

    update

--- about.php
+++ about.php
@@ -26,6 +26,8 @@
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="product/css/style.css">
     <link rel="stylesheet" href="product/css/responsive.css">
+    <link href="css/custom.css" rel="stylesheet">
+
 </head>
 
 <body>
